d70c50fcf821bffa25926a8534a1c0f692fb11d5
DaytonSteffeny <35751232+DaytonSteffeny@users.noreply.github.com>
2018-05-07 19:51:13 -0500

    Finished

--- FinalProject/FinalProject/ViewController.swift
+++ FinalProject/FinalProject/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SumLabel: UILabel!
+    
+    @IBOutlet weak var leftSide: UIImageView!
+    
+    @IBOutlet weak var rightSide: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func clickToRoll(_ sender: UIButton) {
+        
+        let firstNum = arc4random_uniform(6) + 1
+        let secondNum = arc4random_uniform(6) + 1
+        
+        SumLabel.text = "The sum is: \(firstNum + secondNum)"
+        
+        leftSide.image = UIImage(named: "Dice\(firstNum)")
+        rightSide.image = UIImage(named: "Dice\(secondNum)")
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
